a55e57ab4bcef4cc838fcb2e34076447c0fc1c25
Atala2000 <atalakidi@gmail.com>
2023-11-02 21:55:48 +0300

    14 and 15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,42 +1,55 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures balance
- * @tree: tree
+ * binary_tree_height -function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0
+ *
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lheight;
+	size_t rheight;
+
+	if (tree == NULL || (tree->left ==  NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+	lheight = binary_tree_height(tree->left);
+	rheight = binary_tree_height(tree->right);
+
+	if (lheight > rheight)
+	{
+		return (lheight + 1);
+	}
+	else
+	{
+		return (rheight + 1);
+	}
+}
+
+/**
+ * binary_tree_balance - calculates balance factor
+ * @tree: pointer
  * Return: balance factor
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int height_left, height_right;
+	size_t hei_left = 0;
+	size_t hei_right = 0;
 
-    if (!tree)
-    {
-        return 0;
-    }
-    height_left = tree->left ? (int)binary_tree_height(tree->left) : -1;
-    height_right = tree->right ? (int)binary_tree_height(tree->right) : -1;
-
-    return (height_left - height_right);
-}
-
-/**
- * binary_tree_height - gets height 
- * 
- * @tree: tree 
- * return: size_t 
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-    size_t height_left = 0;
-    size_t height_right  = 0;
-
-    if (!tree)
-    {
-        return (0);
-    }
-    height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-    return (height_left > height_right ? height_left : height_right);
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->left)
+	{
+		hei_left = 1 + binary_tree_balance(tree->left);
+	}
+	if (tree->right)
+	{
+		hei_right = 1 + binary_tree_balance(tree->right);
+	}
+	return (hei_left - hei_right);
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,28 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * binary-tree-is-full: checks if binary is full
- * @tree: tree 
- * Return: int 
+ * binary_tree_is_full - checks if tree is full
+ * @tree: pointer
+ * Return: 1 if full
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
-    if (!tree->right && !tree->left)
-    {
-        return (1);
-    }
+	if (!tree)
+	{
+	return (0);
+	}
+	if (!tree->left && !tree->right)
+	{
+	return (1);
+	}
+	if (!tree->left || !tree->right)
+	{
+	return (0);
+	}
 
-    if (tree->right && tree->left)
-    {
-        return (binary_tree_is_full(tree->left) &&
-        binary_tree_is_full(tree->right));
-    }
-
-    return (0);
+	return (binary_tree_is_full(tree->left) &&
+		binary_tree_is_full(tree->right));
 }
