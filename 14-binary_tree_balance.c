d8824e4615f11544a378b9b3f47c90f526bbcb19
Atala2000 <atalakidi@gmail.com>
2023-11-02 22:28:32 +0300

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,8 +8,8 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lheight;
-	size_t rheight;
+	size_t lheight = 0;
+	size_t rheight = 0;
 
 	if (tree == NULL || (tree->left ==  NULL && tree->right == NULL))
 	{
