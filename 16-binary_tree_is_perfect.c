7553f99dc17ba64441f94d525be232c49017634d
Atala2000 <atalakidi@gmail.com>
2023-11-02 22:04:13 +0300

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,47 +1,48 @@
 #include "binary_trees.h"
+/**
+ * cmp_depth - compares depth
+ *
+ * @tree: tree
+ * Return: size
+ */
 
+int cmp_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	else
+	{
+		return (cmp_depth(tree->left) + 1 + cmp_depth(tree->right));
+	}
+}
 /**
  * binary_tree_is_perfect - checks if tree is perfect
- * 
- * 
+ * @tree: node
+ * Return: 1
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
-    size_t power = 0;
-    size_t height = 0;
+	int hei_left = 0;
+	int hei_right = 0;
 
-    if (!tree)
-    {
-        return (0);
-    }
+	if (!tree)
+	{
+		return (0);
+	}
 
-    if (!tree->right && !tree->left)
-    {
-        return (1);
-    }
+	if (tree && (!tree->left && !tree->right))
+	{
+		return (1);
+	}
 
-    height = bina
-}
+	hei_left = cmp_depth(tree->left);
+	hei_right = cmp_depth(tree->right);
 
-/**
- * binary_tree_height - gets height 
- * 
- * @tree: tree 
- * return: size_t 
- */
+	if ((hei_left - hei_right) == 0)
+	{
+		return (1);
+	}
 
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-    size_t height_left = 0;
-    size_t height_right  = 0;
-
-    if (!tree)
-    {
-        return (0);
-    }
-    height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-    return (height_left > height_right ? height_left : height_right);
+	return (0);
 }
