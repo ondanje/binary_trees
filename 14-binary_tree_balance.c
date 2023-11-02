c684f6f80c5f3d9ea3e8a9578087b9e7d53b1678
Atala2000 <atalakidi@gmail.com>
2023-11-02 12:37:43 +0300

    9 and 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_balance - measures balance
+ * @tree: tree
+ * Return: balance factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int height_left, height_right;
+
+    if (!tree)
+    {
+        return 0;
+    }
+    height_left = tree->left ? (int)binary_tree_height(tree->left) : -1;
+    height_right = tree->right ? (int)binary_tree_height(tree->right) : -1;
+
+    return (height_left - height_right);
+}
+
+/**
  * binary_tree_height - gets height 
  * 
  * @tree: tree 
