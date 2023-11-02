090d08c9866944f5d27c7f20b69520f37450c55b
ondanje <cyrilondanje@gmail.com>
2023-11-02 20:01:20 +0300

    function to check for a list

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * binary_tree_is_leaf -
+ * binary_tree_is_leaf - function that checks if a node is a leaf
  * @node: pointer to the node to check
  * Return: return 1 if node is a leaf, otherwise 0
 */
