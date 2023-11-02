99e4e5cde504a5a40fc8b2e1598224961b731a36
Atala2000 <atalakidi@gmail.com>
2023-11-02 19:13:17 +0300

    changed header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,12 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 #include<stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -42,5 +48,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-inary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 #endif
